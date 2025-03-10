b36687d56ecd99e3662b729b77eddc5f3880764c
Vinicius Ribeiro <vinii.viniciusribeiro@gmail.com>
2025-03-10 17:08:32 -0300

    fix(): Ajustando problemas de import

--- src/app/components/completion-message/completion-message.component.ts
+++ src/app/components/completion-message/completion-message.component.ts
@@ -1,47 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { TextProcessingService } from '../../services/text-processing.service';
 
 @Component({
-  selector: 'app-word-display',
+  selector: 'app-completion-message',
   standalone: true,
-  imports: [CommonModule, FormsModule],
-  templateUrl: './word-display.component.html',
-  styleUrl: './word-display.component.css'
+  imports: [CommonModule],
+  templateUrl: './completion-message.component.html',
+  styleUrl: './completion-message.component.css'
 })
-export class WordDisplayComponent {
-  typedWord: string = '';
-  showError: boolean = false;
-  
+export class CompletionMessageComponent {
   constructor(public textService: TextProcessingService) { }
-  
-  // Verifica a palavra quando o usuário pressiona Enter
-  checkWord() {
-    if (this.typedWord.trim()) {
-      const isCorrect = this.textService.checkWord(this.typedWord);
-      
-      if (isCorrect) {
-        // Limpa o campo se estiver correto
-        this.typedWord = '';
-        this.showError = false;
-      } else {
-        // Mostra o erro visual
-        this.showError = true;
-        
-        // Remove o erro visual após 1 segundo
-        setTimeout(() => {
-          this.showError = false;
-        }, 1000);
-      }
-    }
-  }
-  
-  // Método para capturar o evento de teclado (Enter)
-  onKeyPress(event: KeyboardEvent) {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      this.checkWord();
-    }
-  }
 }
